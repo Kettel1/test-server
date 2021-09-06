03201643be3b6f9e303ea6176dce1292073290f4
Kettel <75881317+Kettel1@users.noreply.github.com>
2021-09-06 09:17:48 +0300

    Стили корзины

--- source/js/cart.js
+++ source/js/cart.js
@@ -5,14 +5,21 @@ const cartDataRender = async () => {
     const response = await fetch(urlCart);
     const data = await response.json();
 
-    const cart = document.querySelector('.cart');
+    const cart = document.querySelector('.cart__content');
 
+
+    if (data.length === 0) {
+        const h2 = document.createElement('h2');
+        h2.innerHTML = 'Корзина пуста!';
+        cart.append(h2)
+    }
 
     for (const item of data) {
+
         const div = document.createElement('div');
         div.classList.add('cart__inner');
 
-        div.innerHTML = `<div class="cart__wrapper">
+        div.innerHTML = `
             <img src="img/catalog-image.png" class="cart__good-img">
             <div class="cart__good-wrapper">
 
@@ -24,18 +31,36 @@ const cartDataRender = async () => {
                 </div>
 
                 <div class="count-buttons">
-                    <button class="count-buttons__button count-buttons__button_minus">+</button>
-                    <input class="count-buttons__input">
-                    <button class="count-buttons__button count-buttons__button_plus">-</button>
+                    <button class="count-buttons__button count-buttons__button_minus">-</button>
+                    <input value="${item.count}" class="count-buttons__input">
+                    <button class="count-buttons__button count-buttons__button_plus">+</button>
                 </div>
 
-                <p class="cart__good-price">2000</p>
-            </div>
-        </div>`
+                <p class="cart__good-price">${item.price}</p>
+            </div>`
 
         cart.append(div);
     }
-}
 
+    // Total price
+    const totalPriceValueSelectors = Array.from(document.querySelectorAll('.cart__good-price'));
+    let totalPriceValue = 0;
+    for (const item of totalPriceValueSelectors) {
+        const arrValue = +item.innerHTML.match(/(\d)/g).join('')
+        totalPriceValue += arrValue
+    }
+
+    document.querySelector('.cart__total-count').innerHTML = totalPriceValueSelectors.length;
+    document.querySelector('.cart__total-value').innerHTML = totalPriceValue + " ₽";
+
+
+// document.addEventListener('click', function (e) {
+//     if (e.target.classList.contains("count-buttons__button_plus")) {
+//         ++e.target.parentElement.querySelector("input").value;
+//     } else if (e.target.classList.contains("count-buttons__button_minus")) {
+//         --e.target.parentElement.querySelector("input").value;
+//     }
+// })
+}
 
 cartDataRender()
--- source/js/script.js
+++ source/js/script.js
@@ -1,4 +1,3 @@
-const itemCartCount = document.querySelector('.cart__count');
 import {urlCart, url} from "./url.js";
 
 
@@ -7,12 +6,12 @@ import {urlCart, url} from "./url.js";
     try {
         const response = await fetch(url);
         const data = await response.json();
+        document.querySelector('.cart__count').innerHTML = await getCountGood();
         await renderInfo(data)
     } catch (e) {
         console.log(e)
     }
 })();
-
 
 const checkGoodInCart = async (id) => {
     try {
@@ -24,30 +23,16 @@ const checkGoodInCart = async (id) => {
     }
 };
 
-const getCountGood = () => {
-    let counter = 0;
-    const countCart = getCartData();
-    for (const item in countCart) {
-        counter++;
-    }
-    itemCartCount.innerHTML = counter;
+const getCountGood = async () => {
+    const response = await fetch(urlCart);
+    const data = await response.json();
+    return data.length
 }
-
-const getCartData = () => {
-    return JSON.parse(localStorage.getItem('cart'));
-}
-
-const setCartData = (data) => {
-    localStorage.setItem('cart', JSON.stringify(data));
-    return false;
-}
-
-getCountGood()
 
 const renderInfo = async (products) => {
     const product = document.querySelector('.product');
-    const maxItems = 3;
-    const loadItems = 3;
+    const maxItems = 6;
+    const loadItems = 6;
 
     // Render good
     for (const item of products) {
@@ -70,7 +55,7 @@ const renderInfo = async (products) => {
 
         <div class="product__info">
             <h2 class="product__title">${item.title}</h2>
-            <p class="product__price"><span class="product__price__value">${new Intl.NumberFormat('ru-RU').format(item.price)} руб.</span></p>
+            <p class="product__price"><span class="product__price__value">${setRubles(item.price)}</span></p>
             <p class="product__size">${item.size}</p>
         </div>
 
@@ -79,13 +64,13 @@ const renderInfo = async (products) => {
               ${getProductPhotos(item.productPhotos)}
             <div class="product__button__wrapper">
                 ${await checkGoodInCart(item.id)
-                    .then(r => {
-                         if (r) {
-                            return '<button type="submit" class="product__cart--added">В корзине</button>'
-                         } else {
-                             return '<button type="submit" class="product__cart"><span class="visually-hidden">Добавить в корзину</span></button>'
-                         }
-                        })}
+            .then(r => {
+                if (r) {
+                    return '<button type="submit" class="product__cart--added">В корзине</button>'
+                } else {
+                    return '<button type="submit" class="product__cart"><span class="visually-hidden">Добавить в корзину</span></button>'
+                }
+            })}
             </div>
         </div>`
         product.append(div);
@@ -110,42 +95,32 @@ const renderInfo = async (products) => {
             })
     })
 
+
     const itemBox = document.querySelectorAll('.product__wrapper');
 
     itemBox.forEach((good) => {
-        good.addEventListener('click', e => {
-            if (!good.hasAttribute('data-card', 'true')) {
-                good.setAttribute('data-card', 'true')
-                good.disabled = true;
-                const cartData = getCartData() || {}
+        good.addEventListener('click', async e => {
+            if (e.target.className === 'product__cart' && e.target.tagName === 'BUTTON') {
                 const parentBox = e.target.closest('.product__wrapper');
                 const itemId = parentBox.getAttribute('id');
                 const itemTitle = parentBox.querySelector('.product__title').innerHTML;
                 const itemPrice = parentBox.querySelector('.product__price__value').innerHTML
                 const itemBtn = parentBox.querySelector('.product__cart')
                 const itemImage = parentBox.querySelector('.product__img').src;
-                console.log(itemImage)
+
                 itemBtn.classList.replace('product__cart', 'product__cart--added')
-                submit('POST', {
+                await submit('POST', {
                     "id": itemId,
                     "title": itemTitle,
                     "price": itemPrice,
                     "count": 1,
                     "itemImage": itemImage,
                 })
+
+                const cartCount = document.querySelector('.cart__count');
+                cartCount.innerHTML = await getCountGood()
                 itemBtn.innerHTML = 'В корзине';
-
-                if (cartData.hasOwnProperty(itemId)) {
-                    cartData[itemId][3] += 1;
-                } else {
-                    cartData[itemId] = [itemTitle, itemPrice, itemId, 1];
-                }
-
-                if (!setCartData(cartData)) {
-                    good.disabled = false;
-                }
             }
-
         })
     })
 }
@@ -159,67 +134,7 @@ const submit = async (method, body) => {
         },
         body: JSON.stringify(body),
     })
-
-    let result = await response.json();
 }
-
-
-
-//Корзина
-
-
-// const openCart = () => {
-//     const cartWrapper = document.querySelector('.cart');
-//     const table = document.createElement('table');
-//     table.classList.add('cart__good')
-//     const cartEmptyMessage = document.querySelector('.cart__empty');
-//     const cartGood = getCartData();
-//     let totalItems = ''
-//
-//     if (cartGood !== null) {
-//         totalItems = '<tr><th>Наименование</th><th>Цена</th><th>Артикул</th><th>Кол-во</th></tr>';
-//
-//         for (const good in cartGood) {
-//             totalItems += '<tr>';
-//             for (let i = 0; i < cartGood[good].length; i++) {
-//                 totalItems += '<td>' + cartGood[good][i] + '</td>';
-//             }
-//             totalItems += '</tr>'
-//         }
-//
-//         table.innerHTML = totalItems
-//         cartWrapper.append(table)
-//     } else {
-//         document.querySelector('.cart__good').remove();
-//     }
-// }
-
-// const addEventClearCart = () => {
-//     const cartCloseBtn = document.querySelector('.cart__clear');
-//     cartCloseBtn.addEventListener('click', function() {
-//         localStorage.removeItem('cart')
-//         document.querySelector('.cart__good').remove();
-//         openCart();
-//     })
-// }
-// addEventClearCart();
-//
-// const addEventOpenCart = () => {
-//     const cartOpenBtn = document.querySelector('.cart__open');
-//     cartOpenBtn.addEventListener('click', function () {
-//         if (cartOpenBtn.innerHTML === 'Открыть корзину') {
-//             cartOpenBtn.classList.add('open')
-//             cartOpenBtn.innerHTML = 'Закрыть корзину'
-//             openCart();
-//         } else {
-//             cartOpenBtn.classList.remove('open')
-//             cartOpenBtn.innerHTML = 'Открыть корзину'
-//             document.querySelector('.cart__good').remove();
-//         }
-//     })
-// }
-// addEventOpenCart()
-
 
 //Фичи
 
@@ -245,4 +160,9 @@ const getProductPhotos = (similarPhoto) => {
                    </a>`
     }
     return result;
+}
+
+const setRubles = (rubles) => {
+    const reformatRubles = rubles.toString().replace(/\B(?=(\d{3})+(?!\d))/g, " ");
+    return reformatRubles + ' ₽'
 }
